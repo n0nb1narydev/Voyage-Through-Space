2225cf823c3c7a50fb09e93ee840ddf20cd034c2
Mel Rumsey <melsrumsey@gmail.com>
2020-08-10 11:35:01 -0500

    Put enemy clones in new parent container

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,6 +6,8 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _enemyContainer;
     void Start()
     {
        StartCoroutine(SpawnRoutine());
@@ -20,8 +22,9 @@ public class SpawnManager : MonoBehaviour
        while(true)
        {   
            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f),7,0);
-           Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
-           yield return new WaitForSeconds (3.0f);
+           GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+           newEnemy.transform.parent = _enemyContainer.transform;
+           yield return new WaitForSeconds (5.0f);
        }
        
 
