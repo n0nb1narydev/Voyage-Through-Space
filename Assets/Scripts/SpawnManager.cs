23fd4278efdcc5ab072a111efc8a0c662f053d20
Mel Rumsey <melsrumsey@gmail.com>
2020-08-10 11:18:28 -0500

    Enemies are now spawning

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    void Start()
+    {
+       StartCoroutine(SpawnRoutine());
+    }
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+       while(true)
+       {   
+           Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f),7,0);
+           Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+           yield return new WaitForSeconds (3.0f);
+       }
+       
+
+    }
+}
