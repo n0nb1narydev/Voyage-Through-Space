941cf573fe86156938ac26444021432490edc863
Mel Rumsey <melsrumsey@gmail.com>
2020-08-10 13:10:25 -0500

    Add player and enemy sprites 2D

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -21,7 +21,7 @@ public class Enemy : MonoBehaviour
         }
 
     }
-     private void OnTriggerEnter(Collider other) 
+     private void OnTriggerEnter2D(Collider2D other) 
     {
         // Debug.Log("Hit: " + other.transform.name);
         if(other.tag == "Player")
