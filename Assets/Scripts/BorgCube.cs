ebd5be61db281c3dbd02fcdbb1852bf6f5c970f1
Mel Rumsey <melsrumsey@gmail.com>
2020-08-10 15:56:18 -0500

    Borg Cube boss added

--- Assets/Scripts/BorgCube.cs
+++ Assets/Scripts/BorgCube.cs
@@ -6,6 +6,8 @@ public class BorgCube : MonoBehaviour
 {
     [SerializeField]
         private int _lives = 20;
+    [SerializeField]
+        private float _speed = 2f; 
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,29 @@ public class BorgCube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate( Vector3.down * _speed * Time.deltaTime);
+    } 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Laser")
+        {
+            this.Damage();
+            Destroy(other.gameObject);
+        }
+        if(other.tag == "Player")
+        {
+            Destroy(other.gameObject);
+        }
+
+    }
+
+    public void Damage()
+    {
+        _lives--;
+
+        if(_lives < 1)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
